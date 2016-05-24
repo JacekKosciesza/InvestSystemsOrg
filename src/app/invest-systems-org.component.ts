1d7643aefe43215679ca3904e9818aae74c6f022
Jacek Kościesza <jacek.kosciesza@gmail.com>
2016-05-24 15:47:55 +0200

    Companies list with MdCard

--- src/app/invest-systems-org.component.ts
+++ src/app/invest-systems-org.component.ts
@@ -4,14 +4,15 @@ import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
 import { MdToolbar } from '@angular2-material/toolbar';
 import { MdButton } from '@angular2-material/button';
 
-import { RuleOneComponent } from './rule-one.component'
+import { RuleOneComponent } from './rule-one.component';
+import { CompaniesListComponent } from './companies-list.component';
 
 @Component({
   moduleId: module.id,
   selector: 'invest-systems-org-app',
   templateUrl: 'invest-systems-org.component.html',
   styleUrls: ['invest-systems-org.component.css'],
-  directives: [MD_SIDENAV_DIRECTIVES, MdToolbar, MdButton, RuleOneComponent]
+  directives: [MD_SIDENAV_DIRECTIVES, MdToolbar, MdButton, RuleOneComponent, CompaniesListComponent]
 })
 export class InvestSystemsOrgAppComponent implements OnInit {
   title: String = 'Invest Systems';
--- src/system-config.ts
+++ src/system-config.ts
@@ -32,6 +32,11 @@ const packages: any = {
       format: 'cjs',
       defaultExtension: 'js',
       main: 'button.js'
+    },
+    '@angular2-material/card': {
+      format: 'cjs',
+      defaultExtension: 'js',
+      main: 'card.js'
     }
 };
 
