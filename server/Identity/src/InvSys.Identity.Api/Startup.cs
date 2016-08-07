d75d824d89107d9915c2002017b0a87f904813d8
Jacek Kościesza <jacek.kosciesza@gmail.com>
2016-08-07 09:16:26 +0200

    Identity seed data

--- server/Identity/src/InvSys.Identity.Api/Startup.cs
+++ server/Identity/src/InvSys.Identity.Api/Startup.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using InvSys.Identity.State.EntityFramework;
+using InvSys.Identity.State.EntityFramework.Seed;
+using InvSys.Identity.Core.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace InvSys.Identity.Api
 {
@@ -29,15 +33,23 @@ namespace InvSys.Identity.Api
         {
             // Add framework services.
             services.AddMvc();
+            services.AddIdentity<User, IdentityRole>(config =>
+            {
+                config.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<IdentityContext>();
+            services.AddDbContext<IdentityContext>();
+            services.AddTransient<IdentityContextSeedData>();
+            services.AddTransient<IdentityContextSeedData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IdentityContextSeedData seeder)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
             app.UseMvc();
+            seeder.EnsureSeedData().Wait();
         }
     }
 }
---
+++ server/Identity/src/InvSys.Identity.State/EntityFramework/Seed/IdentityContextSeedData.cs
@@ -0,0 +1,31 @@
+﻿using InvSys.Identity.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace InvSys.Identity.State.EntityFramework.Seed
+{
+    public class IdentityContextSeedData
+    {
+        private readonly UserManager<User> _userManager;
+
+        public IdentityContextSeedData(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task EnsureSeedData()
+        {
+            if (await _userManager.FindByEmailAsync("jacek.kosciesza@gmail.com") == null)
+            {
+                
+                var user = new User
+                {
+                    UserName = "jkosciesza",
+                    Email = "jacek.kosciesza@gmail.com",
+                    IsProfessionalInvestor = false
+                };
+                await _userManager.CreateAsync(user, "P@ssw0rd!");
+            }
+        }
+    }
+}
