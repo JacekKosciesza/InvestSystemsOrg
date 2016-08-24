88baf37fb423b3c4de0a081be0d7e57d3c53b857
Jacek Kościesza <jacek.kosciesza@gmail.com>
2016-08-24 08:25:06 +0200

    Ionic tutorial

--- client/hybrid/app/app.ts
+++ client/hybrid/app/app.ts
@@ -1,24 +1,46 @@
-import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {ionicBootstrap, Platform, MenuController, Nav} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
-import {TabsPage} from './pages/tabs/tabs';
+import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
+import {ListPage} from './pages/list/list';
 
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  templateUrl: 'build/app.html'
 })
-export class MyApp {
+class MyApp {
+  @ViewChild(Nav) nav: Nav;
 
-  private rootPage: any;
+  // make HelloIonicPage the root (or first) page
+  rootPage: any = HelloIonicPage;
+  pages: Array<{title: string, component: any}>;
 
-  constructor(private platform: Platform) {
-    this.rootPage = TabsPage;
+  constructor(
+    public platform: Platform,
+    public menu: MenuController
+  ) {
+    this.initializeApp();
 
-    platform.ready().then(() => {
+    // set our app's pages
+    this.pages = [
+      { title: 'Hello Ionic', component: HelloIonicPage },
+      { title: 'My First List', component: ListPage }
+    ];
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
+  }
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(page.component);
   }
 }
 
