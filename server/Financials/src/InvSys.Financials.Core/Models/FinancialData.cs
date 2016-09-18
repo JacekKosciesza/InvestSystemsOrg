f6ff4d7a4454bb14bc927da532ba6855aad96799
Jacek Kościesza <jacek.kosciesza@gmail.com>
2016-09-18 12:16:28 +0200

    Nullable types

--- server/Financials/src/InvSys.Financials.Core/Models/FinancialData.cs
+++ server/Financials/src/InvSys.Financials.Core/Models/FinancialData.cs
@@ -13,18 +13,18 @@ namespace InvSys.Financials.Core.Models
         public string Currency { get; set; }
 
         // Income Statement
-        public decimal Revenue { get; set; }
-        public double EPS { get; set; } // Earnings Per Share
+        public decimal? Revenue { get; set; }
+        public double? EPS { get; set; } // Earnings Per Share
 
         // Balance Sheet
-        public decimal Equity { get; set; } // Book value or Net worth
-        public decimal Debt { get; set; }
+        public decimal? Equity { get; set; } // Book value or Net worth
+        public decimal? Debt { get; set; }
 
         // Cash Flow
-        public decimal Cash { get; set; } // Free Cash Flow
-        public decimal Dividends { get; set; }
+        public decimal? Cash { get; set; } // Free Cash Flow
+        public decimal? Dividends { get; set; }
 
         // Ratios
-        public double ROIC { get; set; } // Return On Capital
+        public double? ROIC { get; set; } // Return On Capital
     }
 }
---
+++ server/Financials/src/InvSys.Financials.State/EntityFramework/Migrations/20160918101225_Nullable.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InvSys.Financials.State.EntityFramework.Migrations
+{
+    public partial class Nullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Revenue",
+                table: "Data",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "ROIC",
+                table: "Data",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Equity",
+                table: "Data",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "EPS",
+                table: "Data",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Dividends",
+                table: "Data",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Debt",
+                table: "Data",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Cash",
+                table: "Data",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Revenue",
+                table: "Data",
+                nullable: false);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "ROIC",
+                table: "Data",
+                nullable: false);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Equity",
+                table: "Data",
+                nullable: false);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "EPS",
+                table: "Data",
+                nullable: false);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Dividends",
+                table: "Data",
+                nullable: false);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Debt",
+                table: "Data",
+                nullable: false);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Cash",
+                table: "Data",
+                nullable: false);
+        }
+    }
+}
