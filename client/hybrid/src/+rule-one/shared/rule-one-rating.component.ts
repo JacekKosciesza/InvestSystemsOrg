90367f23e98cc66a67bbddda0dd40eae6578f53d
Jacek Kościesza <jacek.kosciesza@gmail.com>
2016-10-11 16:52:38 +0200

    Rule #1 summary icons

--- client/hybrid/src/+rule-one/shared/rule-one-rating.component.ts
+++ client/hybrid/src/+rule-one/shared/rule-one-rating.component.ts
@@ -2,36 +2,17 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'rule-one-rating',
-    template:
-    `
-    <!--<ion-badge style="float:right;">75%</ion-badge>
-    <ion-icon *ngIf="isWonderful" style="float:right; color:green;" name="thumbs-up"></ion-icon>
-    <ion-icon *ngIf="isWonderful === false" style="float:right; color:red;" name="thumbs-down"></ion-icon>
-    <ion-icon *ngIf="isWonderful === null || isWonderful === undefined" style="float:right; color:grey;" name="help"></ion-icon>-->
-
-    <span *ngIf="isWonderful" style="color:green;">#1</span>
-    <span *ngIf="isWonderful === false" style="color:red;">#1</span>
-    <!--<span *ngIf="isWonderful === null || isWonderful === undefined" style="color:grey;">#1</span>-->
-
-    <!--<ion-badge color="green" *ngIf="isWonderful">#1</ion-badge>
-    <ion-badge color="red" *ngIf="isWonderful === false">#1</ion-badge>
-    <ion-badge color="grey" *ngIf="isWonderful === null || isWonderful === undefined">#1</ion-badge>
-
-    <span [ngSwitch]="getRandomInt(1,3)"> 
-        <ion-badge *ngSwitchCase="1" color="green">Buy</ion-badge>
-        <ion-badge *ngSwitchCase="2" color="orange">Hold</ion-badge>
-        <ion-badge *ngSwitchCase="3" color="red">Sell</ion-badge>
-    </span>-->
-    `,
-    styles: [`
-        :host {
-            display:inline-block;
-        }
-    `]
+    templateUrl: 'rule-one-rating.component.html'
 })
 export class RuleOneRatingComponent implements OnInit {
     @Input() isWonderful: boolean;
-    constructor() { }
+    public currentPriceGood: number;
+    public currentPriceTrend: number;
+    
+    constructor() {
+        this.currentPriceGood = this.getRandomInt(1,3);
+        this.currentPriceTrend = this.getRandomInt(1,2);
+    }
 
     ngOnInit() { }
 
