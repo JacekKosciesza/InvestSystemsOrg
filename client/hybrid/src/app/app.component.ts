3485793577cb4bd4c8f00f3863f47409c027056d
Jacek Kościesza <jacek.kosciesza@gmail.com>
2016-10-12 14:45:17 +0200

    Three circles (SVG); i18n translations

--- client/hybrid/src/app/app.component.ts
+++ client/hybrid/src/app/app.component.ts
@@ -29,7 +29,7 @@ export class InvestSystems implements OnInit {
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('en');
     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    //translate.use('pl');
+    translate.use('pl');
 
     translate.get(['Dashboard', 'Companies', 'Settings', 'Tutorial', 'About']).subscribe((t: string[]) => {
       // set our app's pages
