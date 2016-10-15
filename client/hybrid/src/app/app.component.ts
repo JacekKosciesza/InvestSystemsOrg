b8b44e396150526b65330dd45651892b34928dec
Jacek Kościesza <jacek.kosciesza@gmail.com>
2016-10-15 15:42:43 +0200

    Watchlist

--- client/hybrid/src/app/app.component.ts
+++ client/hybrid/src/app/app.component.ts
@@ -15,7 +15,7 @@ import { Settings } from '../shared';
 })
 export class InvestSystems implements OnInit {
   @ViewChild(Nav) nav: Nav;
-  rootPage = CompanyListComponent;
+  rootPage = DashboardComponent;
   pages: Array<{icon: string, title: string, component: any }>;
   theme: string;
 
