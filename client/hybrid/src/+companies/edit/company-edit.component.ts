77b39bcdb0530049793037cd93fd22a306b5ddd4
Jacek Kościesza <jacek.kosciesza@gmail.com>
2016-10-19 17:03:23 +0200

    Add websites

--- client/hybrid/src/+companies/edit/company-edit.component.ts
+++ client/hybrid/src/+companies/edit/company-edit.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Platform, NavParams, ViewController } from 'ionic-angular';
 
+import { Website, WebsiteType } from '../shared';
+
 @Component({
-    selector: 'company-edit',
-    templateUrl: 'company-edit.component.html'
+  selector: 'company-edit',
+  templateUrl: 'company-edit.component.html'
 })
-export class CompanyEditComponent {
+export class CompanyEditComponent implements OnInit {
   company;
+  website;
 
   constructor(
-      public platform: Platform,
-      public params: NavParams,
-      public viewCtrl: ViewController
+    public platform: Platform,
+    public params: NavParams,
+    public viewCtrl: ViewController
   ) {
     this.company = this.params.get('company');
+    this.website = new Website();
+  }
+
+  ngOnInit() {
+    this.company.websites = [];
+  }
+
+  add(website: Website) {
+    if (website && website.type && website.link) {
+      this.company.websites.push(website);
+    }
+  }
+
+  remove(website: Website) {
+    var index = this.company.websites.findIndex(a => a === website);
+    if (index > -1) {
+      this.company.websites.splice(index, 1);
+    }
   }
 
   dismiss() {
--- client/hybrid/src/+companies/shared/website.model.ts
+++ client/hybrid/src/+companies/shared/website.model.ts
@@ -12,5 +12,25 @@ export enum WebsiteType {
 }
 
 export class Website {
-    constructor(public type: WebsiteType, public link: string) { }
+    constructor(public type?: WebsiteType, public link?: string) { }
+
+    public get icon(): string {
+        switch (this.type) {
+            case WebsiteType.Home:
+                return this.varToEnumStringVal(this.type, WebsiteType);
+            default:
+                return `logo-${this.varToEnumStringVal(this.type, WebsiteType).toLowerCase()}`;
+        }
+    }
+
+    varToEnumStringVal(value: any, enumerator: any): string {
+        var en: { [index: string]: any } = enumerator;
+
+        if (!isNaN(parseInt(value)))
+            return en[value];
+
+        var num = en[value];
+
+        return en[num];
+    }
 }
